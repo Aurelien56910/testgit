09db3e05d2c564d6a40692fc75cfbcd44d787c36
MAUGE <MAUGE@www.stsaunnefit.com>
2014-02-25 09:23:28 +0100

    version 2 XML

--- TestJDOM/src/fr/mauge/affichageXML.java
+++ TestJDOM/src/fr/mauge/affichageXML.java
@@ -4,6 +4,7 @@ public class affichageXML {
 	public static void main(String[] args){
 		DocXML.creerXML();
 		DocXML.affiche();
-	
+		DocXML.enregistre("Fichier XML");
+		DocXML.deXMLLaJDOM("Fichier JDOM");
 	}
 }
